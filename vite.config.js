fd7e81fd828102bd49fc7e1849f64c80686af7cb
Yao Hui <yaohui.soh.2022@scis.smu.edu.sg>
2025-11-16 16:16:20 +0800

    watch polling

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 export default {
   server: {
     host: true,
-    allowedHosts: ['.paypilotgo.com']
+    allowedHosts: ['.paypilotgo.com'],
+    hmr: {
+      clientPort: 443
+    },
+    watch: {
+      usePolling: true,
+      interval: 1000
+    }
   }
 }
